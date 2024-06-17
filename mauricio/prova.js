f0060ae7cf1bf59b8e32e138e68ee56dba1d3f19
mauriciocunhalus <143563061+mauriciocunhalus@users.noreply.github.com>
2024-06-17 20:12:56 -0300

    Add files via upload

---
+++ mauricio/prova.js
@@ -0,0 +1,15 @@
+const body = document.querySelector('body');
+body.style.height = '100vh';
+body.style.overflow = 'hiden'
+body.style.backgroundcolor = 'black';
+body.style.display = 'flex';
+body.style.justifycontent = 'center';
+body.style.alignItenms = 'center';
+
+const body = document.creatElement('div');
+body.style.backgroundcolor = 'gray';
+body.style.width = '300px'
+body.style.height = '300px';
+body.style.display = 'flex';
+body.style.justifycontent = 'center';
+body.style.alignItenms = 'center';
