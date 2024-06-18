658d24a9487a35f86a87181c249bdd37a63741fb
mauriciocunhalus <143563061+mauriciocunhalus@users.noreply.github.com>
2024-06-17 21:19:21 -0300

    Update prova.js

--- mauricio/prova.js
+++ mauricio/prova.js
@@ -7,9 +7,32 @@ body.style.justifycontent = 'center';
 body.style.alignItenms = 'center';
 
 const body = document.creatElement('div');
-body.style.backgroundcolor = 'gray';
-body.style.width = '300px'
-body.style.height = '300px';
-body.style.display = 'flex';
-body.style.justifycontent = 'center';
-body.style.alignItenms = 'center';
+div.style.backgroundcolor = 'gray';
+div.style.width = '300px'
+div.style.height = '300px';
+div.style.display = 'flex';
+div.style.justifycontent = 'center';
+div.style.alignItems = 'center';
+div.appendChild(div);
+
+const div2 = document.createElement('div2');
+div2.style.display = 'flex';
+div2.style.justifyContent = 'center';
+div2.style.alignContent = 'center';
+div2.style.flexDirection = 'colum'
+div2.style.backgroundColor = 'white';
+div2.style.width = '150px';
+div2.style.height = '150px';
+div.appendChild(div2);
+
+const textoDiv = document.createElement('h3')
+textoDiv.textContent = 'Olá mundo'
+div2.appendChild(div2);
+
+const button = document.createElement('button');
+button.textContent = 'Clique aqui'
+div2.appendChild(button);
+button.addEventListener ('click,' function (){
+
+    body.style.backgroundColor = 'green';
+    body.style.
