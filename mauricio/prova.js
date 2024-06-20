ad107967d8b727ace449001f639c898a9cc30a7c
Maurício Cunha <143563061+mauriciocunhalus@users.noreply.github.com>
2024-06-20 19:32:02 -0300

    Update prova.js

--- mauricio/prova.js
+++ mauricio/prova.js
@@ -1,38 +1,95 @@
 const body = document.querySelector('body');
-body.style.height = '100vh';
-body.style.overflow = 'hiden'
-body.style.backgroundcolor = 'black';
+body.style.backgroundColor = 'white';
 body.style.display = 'flex';
-body.style.justifycontent = 'center';
-body.style.alignItenms = 'center';
+body.style.justifyContent = 'center';
+body.style.alignItems = 'center';
+body.style.minHeight = '100vh';
+body.style.overflow = 'hidden';
 
-const body = document.creatElement('div');
-div.style.backgroundcolor = 'gray';
-div.style.width = '300px'
-div.style.height = '300px';
-div.style.display = 'flex';
-div.style.justifycontent = 'center';
-div.style.alignItems = 'center';
-div.appendChild(div);
-
-const div2 = document.createElement('div2');
+const div2 = document.createElement('div');
+div2.style.width = '1200px';
+div2.style.height = '600px';
 div2.style.display = 'flex';
 div2.style.justifyContent = 'center';
-div2.style.alignContent = 'center';
-div2.style.flexDirection = 'colum'
-div2.style.backgroundColor = 'white';
-div2.style.width = '150px';
-div2.style.height = '150px';
-div.appendChild(div2);
+div2.style.alignItems = 'center';
+div2.style.backgroundColor = 'gray';
+body.appendChild(div2);
 
-const textoDiv = document.createElement('h3')
-textoDiv.textContent = 'Olá mundo'
-div2.appendChild(div2);
+const div_form = document.createElement('div');
+const frm = document.createElement('form');
 
-const button = document.createElement('button');
-button.textContent = 'Clique aqui'
-div2.appendChild(button);
-button.addEventListener ('click,' function (){
+const inputElementText = document.createElement('input');
+const inputElementBr = document.createElement('br');
+const inputElementNumber1 = document.createElement('input');
+const inputElementBr1 = document.createElement('br');
+const inputElementNumber2 = document.createElement('input');
+const inputElementBr2 = document.createElement('br');
+const inputElementNumber3 = document.createElement('input');
+const inputElementBr3 = document.createElement('br');
+const inputElementSubmit = document.createElement('input');
+const inputElementBr4 = document.createElement('br');
+const inputElementBr6 = document.createElement('br');
+const inputElementBr5 = document.createElement('h3');
 
-    body.style.backgroundColor = 'green';
-    body.style.
+
+inputElementText.type = 'text';
+inputElementNumber1.type = 'number';
+inputElementNumber2.type = 'number';
+inputElementNumber3.type = 'number';
+inputElementSubmit.type = 'submit';
+
+inputElementText.placeholder = 'Digite seu nome';
+inputElementNumber1.placeholder = 'Digite a primeira nota';
+inputElementNumber2.placeholder = 'Digite a segunda nota';
+inputElementNumber3.placeholder = 'Digite a terceira nota';
+inputElementSubmit.value = 'Calcular Média';
+
+inputElementText.id = 'inNome';
+inputElementNumber1.id = 'inNumero1';
+inputElementNumber2.id = 'inNumero2';
+inputElementNumber3.id = 'inNumero3';
+
+
+frm.appendChild(inputElementText);
+frm.appendChild(inputElementBr);
+frm.appendChild(inputElementNumber1);
+frm.appendChild(inputElementBr1);
+frm.appendChild(inputElementNumber2);
+frm.appendChild(inputElementBr2);
+frm.appendChild(inputElementNumber3);
+frm.appendChild(inputElementBr3);
+frm.appendChild(inputElementSubmit);
+frm.appendChild(inputElementBr4);
+frm.appendChild(inputElementBr6);
+frm.appendChild(inputElementBr5);
+
+div_form.appendChild(frm);
+
+div2.appendChild(div_form);
+
+frm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nome = frm.inNome.value;
+    const num1 = Number(frm.inNumero1.value);
+    const num2 = Number(frm.inNumero2.value);
+    const num3 = Number(frm.inNumero3.value);
+    const media = (num1 + num2 + num3) / 3;
+
+    if (media >= 10) {
+        resp.innerHTML = `${nome}. UAU! Parabéns Top, aprovado! Média é ${media.toFixed(1)}`;
+        resp.style.color = 'blue';
+    } else if (media >= 7) {
+        resp.innerHTML = `${nome}. Parabéns, aprovado! Média é ${media.toFixed(1)}`;
+        resp.style.color = 'green';
+    } else if (media <= 3) {
+        resp.innerHTML = `${nome}. Está reprovado. Média é ${media.toFixed(1)}`;
+        resp.style.color = 'red';
+    } else {
+        resp.innerHTML = `${nome}. Recuperação. Média é ${media.toFixed(1)}`;
+        resp.style.color = 'yellow';
+    }
+});
+
+const resp = document.createElement('h3');
+div_form.appendChild(resp);
